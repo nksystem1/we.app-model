dd23bba6e9d4766e381646711324835a66022f91
maitere <62911232+maitere@users.noreply.github.com>
2020-08-16 15:39:38 -0500

    Cambios en Unidad Medida

---
+++ Data/UnidadMedida.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace we.app_model.Data
+{
+    [Serializable]
+    public class UnidadMedida
+    {
+
+		[Required]
+		[MaxLength(3)]
+		public string abrevunidadmedida { get; set; }
+		[Required]
+		[MaxLength(120)]
+		public string nomunidadmedida { get; set; }
+		
+		public string codnegocio { get; set; }
+		
+	}
+}
